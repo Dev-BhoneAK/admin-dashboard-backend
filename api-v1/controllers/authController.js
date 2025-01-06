fe8e96ce467db51b709dfa222fb28cb34329ae07
Bhone Aung Kyaw <bhoneak@outlook.com>
2025-01-06 23:57:30 +0700

    feat: add auth controller

--- api-v1/controllers/authController.js
+++ api-v1/controllers/authController.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
+import bcrypt from "bcrypt";
 import asyncHandler from "express-async-handler";
 import { validationResult } from "express-validator";
+
+import { checkEmail } from "../services/authService.js";
 
 export const login = asyncHandler(async (req, res) => {
   const errors = validationResult(req);
@@ -9,5 +12,19 @@ export const login = asyncHandler(async (req, res) => {
     throw new Error("Validation failed");
   }
 
-  res.send("Login route");
+  const { email, password } = req.body;
+  const admin = await checkEmail(email);
+
+  if (!admin) {
+    res.status(401);
+    throw new Error("Invalid email");
+  }
+
+  // Wrong Password allowed 3 times per day
+  if (admin.status === "freeze") {
+    res.status(401);
+    throw new Error("Your account is temporarily locked.");
+  }
+
+
 });
