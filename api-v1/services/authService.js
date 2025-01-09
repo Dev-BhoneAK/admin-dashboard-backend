bad109a03cb53e067cbb40ad12baddd5d3b5b04c
Bhone Aung Kyaw <bhoneak@outlook.com>
2025-01-09 23:55:00 +0700

    feat: add auth service

--- api-v1/services/authService.js
+++ api-v1/services/authService.js
@@ -1,10 +1,62 @@
 import "dotenv/config";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const checkEmail = async (email) => {
-  return prisma.admin.findUnique({
-    where: { email: email },
-  });
+    return prisma.admin.findUnique({
+        where: { email: email },
+    });
+};
+
+export const generateTokens = async (adminId) => {
+    //creating a access token
+    const accessToken = jwt.sign(
+        {
+            adminId,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "1m",
+        }
+    );
+
+    // Creating refresh token
+    const refreshToken = jwt.sign(
+        {
+            adminId,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "2m" }
+    );
+    return { accessToken, refreshToken };
+};
+
+export const refreshTokens = async (refreshToken) => {
+    try {
+        const admin = jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        );
+        const adminId = admin.adminId;
+
+        const accessToken = jwt.sign(
+            { adminId },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "1m" }
+        );
+
+        return { accessToken };
+    } catch (err) {
+        console.error(err);
+        throw new Error("JWT Verification Failed");
+    }
+};
+
+export const updateAdmin = async (id, adminData) => {
+    return prisma.admin.update({
+        where: { id: id },
+        data: adminData,
+    });
 };
