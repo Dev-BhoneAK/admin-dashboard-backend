83ce7a77b2ce51f8306cb777af1721a3f1e93836
Bhone Aung Kyaw <bhoneak@outlook.com>
2025-01-10 23:35:18 +0700

    fix: change auth routes

--- api-v1/routes/authRoutes.js
+++ api-v1/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { loginValidator } from "../middleware/validators.js";
-import { login } from "../controllers/authController.js";
+import { login, handleTokenRefresh } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", loginValidator, login);
+
+router.post("/refresh-token", handleTokenRefresh);
 
 export default router;
