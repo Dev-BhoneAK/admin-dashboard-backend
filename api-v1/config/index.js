04a431a7865afd5a56ae5d65ad2c41d8e200d76f
Bhone Aung Kyaw <bhoneak@outlook.com>
2025-01-21 23:57:41 +0700

    fix: add renew status

--- api-v1/config/index.js
+++ api-v1/config/index.js
@@ -1,4 +1,4 @@
-export const statuses = ["subscribe", "unsubscribe", "low_balance"];
+export const statuses = ["subscribe", "unsubscribe", "low_balance", "renew"];
 export const providers = ["MPT", "KPay", "ATOM", "WavePay"];
 export const subscribeChannels = ["sms", "web", "app", "ussd"];
 export const operationStatuses = ["pending", "success", "failed"];
